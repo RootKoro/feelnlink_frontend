62c5df0a4dc04f204543ab8b46bca5d714c1c050
noheezybucket <seydinag023@gmail.com>
2024-10-28 18:41:18 +0000

    routing

---
+++ app/(home)/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { SafeAreaView, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Home screen</Text>
+    </View>
+  );
+}
